9965e8ed6ab9b1b9e0517ba720dab9a5467ce0d1
pondckaokppp <pondchanokpp2003@gmail.com>
2023-07-26 18:04:13 +0700

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 "use client";
-
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isfirstload, setisfirstload] = useState(true);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -19,7 +21,28 @@ export default function RandomUserPage() {
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
+    const cleanedUser = users.map((user) => cleanUser(user));
+    setUsers(cleanedUser);
   };
+
+  useEffect(() => {
+    if (isfirstload) {
+      setisfirstload(false);
+      return;
+    }
+    const strGenAmont = JSON.stringify(genAmount);
+    localStorage.setItem("genAmount", strGenAmont);
+  }, [genAmount]);
+
+  useEffect(() => {
+    const strGenAmont = localStorage.getItem("genAmount");
+    if (strGenAmont === "null") {
+      setGenAmount(1);
+      return;
+    }
+    const loadedGenAount = JSON.parse(strGenAmont);
+    setGenAmount(loadedGenAount);
+  }, []);
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
@@ -40,7 +63,17 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            name={user.name}
+            imgUrl={user.imgUrl}
+            address={user.address}
+            email={user.email}
+            key={user.email}
+          />
+        ))}
     </div>
   );
 }
